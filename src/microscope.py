fixed defocus and propagator signs

src/microscope.py
@@ -69,8 +69,8 @@ class Aberration(L.LightningModule):
         dfv = dfv.unsqueeze(1).unsqueeze(2)
         dfang = dfang.unsqueeze(1).unsqueeze(2)
         cs = cs.unsqueeze(1).unsqueeze(2)
-        df = 0.5 * (dfu + dfv + (dfu - dfv) * torch.cos(2 * (ang - dfang)))
-        gamma = torch.pi * w * k2 * (0.5 * cs * w**2 * k2 - df)
+        df = 0.5 * (dfu + dfv + (dfv - dfu) * torch.cos(2 * (ang + dfang)))
+        gamma = -torch.pi * w * k2 * (0.5 * cs * w**2 * k2 - df)
         return gamma
 
     def transfer(self, cs, dfu, dfv, dfang):

src/pdb2mrc.py
@@ -8,16 +8,19 @@ from fft_tools import fftconvolve
 class PDB2MRC:
     """Ice-making machine"""
     
-    def __init__(self, pdb_code):
+    def __init__(self, pdb_code=None, pdb_path=None):
         self.pdb_code = pdb_code
+        self.pdb_path = pdb_path
 
-    def fetch_pdb(self, pdb_folder='./pdb-data/'):
-        # saves PDB file
-        self.pdb_folder = pdb_folder
-        self.pdb_path = pdbtools.fetch_pdb_file(self.pdb_code, output=pdb_folder)
+    def fetch_pdb(self, pdb_folder='./pdb-data/', assemble=True):
+        self.assemble = assemble
+        if self.pdb_code is not None:
+            # saves PDB file
+            self.pdb_folder = pdb_folder
+            self.pdb_path = pdbtools.fetch_pdb_file(self.pdb_code, output=pdb_folder)
 
         # extract atomic coordinates and symbols
-        self.atomic_numbers, self.coords = pdbtools.get_atoms_and_coordinates_from_pdb(self.pdb_path)
+        self.atomic_numbers, self.coords = pdbtools.get_atoms_and_coordinates_from_pdb(self.pdb_path, assemble=assemble)
         self.n_atoms = len(self.coords)
         self.unique_elements = atom.atom_symbol(torch.unique(self.atomic_numbers))
 

src/scattering.py
@@ -99,14 +99,14 @@ class Scattering(L.LightningModule):
 
         # Fresnel transfer function for multislice
         if scattering_model == "multislice":
-            F = torch.exp(1j * torch.pi * self.wavelength * pixel_size * k**2)
+            F = torch.exp(-1j * torch.pi * self.wavelength * pixel_size * k**2)
             self.register_buffer("F", F)
 
         # Fresnel transfer function for first Born
         if scattering_model == "firstborn":
             F = []
             for i in range(n_pixels):
-                f = torch.exp(1j * torch.pi * self.wavelength * pixel_size * 
+                f = torch.exp(-1j * torch.pi * self.wavelength * pixel_size * 
                               (n_pixels - i) * k**2)
                 F.append(f)
             F = torch.stack(F)
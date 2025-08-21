fixed dfang units

src/cryosparc_utils.py
@@ -58,7 +58,6 @@ def extract_parameters_from_csfile(csfile_path,
 
     # extract defocus
     dfang_rad = torch.as_tensor(dataset["ctf/df_angle_rad"])
-    dfang_deg = dfang_rad / torch.pi * 180
     dfu_A = torch.as_tensor(dataset["ctf/df1_A"])
     dfv_A = torch.as_tensor(dataset["ctf/df2_A"])
 
@@ -103,7 +102,7 @@ def extract_parameters_from_csfile(csfile_path,
     tref2 = torch.as_tensor(dataset["ctf/trefoil_A"][:, 1] / 1000)
     
     # build ctf_params
-    ctf_params = torch.stack([cs_A, dfu_A, dfv_A, dfang_deg, beamtiltx_rad, beamtilty_rad, phaseshift_rad, tref1, tref2], dim=-1)
+    ctf_params = torch.stack([cs_A, dfu_A, dfv_A, dfang_rad, beamtiltx_rad, beamtilty_rad, phaseshift_rad, tref1, tref2], dim=-1)
 
     # extract per-particle scale factors
     scale = torch.as_tensor(dataset['alignments3D/alpha'])
5b0669602abc4028b228d363089e6718d56d81ca
Joel Yeo <joelyeois@gmail.com>
2025-09-30 14:24:26 +0800

    clean up code

--- src/pdb2mrc.py
+++ src/pdb2mrc.py
@@ -9,7 +9,7 @@ from scipy.spatial.distance import pdist
 
 class PDB2MRC:
     """Ice-making machine"""
-    
+
     def __init__(self, pdb_code=None, pdb_path=None):
         self.pdb_code = pdb_code
         self.pdb_path = pdb_path
@@ -28,7 +28,7 @@ class PDB2MRC:
         self.n_atoms = len(self.coords)
         self.unique_elements = atom.atom_symbol(torch.unique(self.atomic_numbers))
 
-        # center coordinates onto origin (0,0,0
+        # center coordinates onto origin (0,0,0)
         if center:
             center = pdbtools.center_of_particle(self.coords)
             self.centered_coords = self.coords - center.reshape(1, -1)
@@ -43,7 +43,7 @@ class PDB2MRC:
         hull = ConvexHull(coords)
         hull_points = coords[hull.vertices]
         self.max_diameter = pdist(hull_points).max()
-        
+
 
     def build_particle(self, 
                        n=256, 
@@ -78,40 +78,3 @@ class PDB2MRC:
                 convention="relion",
                 method="snapped-3d",
             )
-
-    def solvate(self,
-                source='torch',
-                filepath='../ice-data/mdsim_f_kernel_averaged_200x200x200_0.5A.pt',
-                min_distance_A=1.5,
-                niter=5
-               ):
-        self.icemaker = Icemaker()
-        self.icemaker.get_mdsim_averaged_f_kernel(filepath, source=source)
-        self.icemaker.interpolate_mdsim_f_kernel(self.n, self.dx)
-        self.icemaker.generate_ice(min_distance=int(min_distance_A//self.dx), niter=niter)
-
-        # add ice to particle
-        self.icecube_deltas = self.icemaker.current_ice_vol.clone()
-        self.icy_particle = self.particle.clone()
-
-        # remove ice from particle regions
-        self.icecube_deltas[self.occupancy] = 0
-
-        # check if atomic potential for O has already been calcuated
-        if 'O' not in self.atomic_potentials:
-            sx, sy, sz, sX, sY, sZ = coordinate_grid_3d(
-                (self.atom_size_px, self.atom_size_px, self.atom_size_px), (self.dx, self.dx, self.dx), convention="torch"
-            )
-            sR = torch.sqrt(sX**2 + sY**2 + sZ**2)
-            O_pot = potential.atomic_potential_3d(8, sR)
-        else:
-            # make sure to bin to correct pixel size
-            super_sampling_factor = int(self.dx / self.atomic_potentials['ssdx'])
-            avgpool3d = torch.nn.AvgPool3d(super_sampling_factor, stride=super_sampling_factor)
-            O_pot = avgpool3d(self.atomic_potentials['O'][None, None]).squeeze() * self.dx
-
-        # convolve
-        self.icecube = fftconvolve(self.icecube_deltas, O_pot, mode='same')
-
-        # make icy particle
-        self.icy_particle += self.icecube
